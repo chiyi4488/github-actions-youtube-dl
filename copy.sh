f4ece7f0d25422566589c810f1f3b79cfe40c230
chiyi4488 <chiyi4869@gmail.com>
2021-02-03 19:12:39 +0800

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rclone copy "./downloads": "DRIVE": --transfers 48 -v --create-empty-src-dirs --fast-list
+rclone copy YT-DL:"./downloads" DRIVE: --transfers 48 -v --create-empty-src-dirs --fast-list
